59521c1479e7686ad4be2b61ff084119b7dc2539
Alisa-Dariana Roman <alisadariana@gmail.com>
2023-04-03 11:11:48 +0300

    main: add menu
    Add operations menu.
    Add src/images/.
    
    Signed-off-by: Alisa-Dariana Roman <alisadariana@gmail.com>

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 	- create demo application
 		- displays a image file
 		- command: "./main <image_name>"
+
+2. Operations Menu
+
+	1 - select image
+
+	2 - display image
--- main.cpp
+++ main.cpp
@@ -1,22 +1,65 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
 using namespace cv;
+
+int display_image(String imagePath)
+{
+	Mat image;
+	image = imread(imagePath, IMREAD_COLOR);
+	if ( !image.data ) {
+		printf("No image data \n");
+		return -1;
+	}
+	namedWindow("Display Image", WINDOW_AUTOSIZE);
+	imshow("Display Image", image);
+	waitKey(0);
+	destroyAllWindows();
+	return 0;
+}
+
 int main(int argc, char** argv )
 {
-    if ( argc != 2 )
-    {
-        printf("usage: main.out <Image_Path>\n");
-        return -1;
-    }
-    Mat image;
-    image = imread( argv[1], IMREAD_COLOR );
-    if ( !image.data )
-    {
-        printf("No image data \n");
-        return -1;
-    }
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", image);
-    waitKey(0);
-    return 0;
+	int op;
+	int ret = 0;
+	String imagesPath;
+	String imageName;
+	String imagePath;
+	char name[255];
+
+	imagesPath = "src/images/";
+	imageName = "lena.jpg";
+	imagePath = imagesPath + imageName;
+
+	do {
+		op = 0;
+		std::cout << "Operation = ";
+		scanf("%d", &op);
+		std::cout << "Selected operation " << op << std::endl;
+
+		switch (op)
+		{
+		case 1:
+			std::cout << "Image name = ";
+			scanf("%s", name);
+			std::cout << "Selected image name = " << name << std::endl;
+			imageName = name;
+			imagePath = imagesPath + imageName;
+			std::cout << "Image path = " << imagePath << std::endl;
+			break;
+		case 2:
+			std::cout << "Displaying image " << imageName << std::endl;
+			ret = display_image(imagePath);
+			if (ret)
+				return -1;
+			break;
+		default:
+			std::cout << "Unknown operation" << std::endl;
+			break;
+		}
+
+		std::cout << std::endl;
+	} while (op != 0);
+
+	std::cout << "End" << std::endl;
+	return 0;
 }
